52d77856fee9fd845bf99a96037a61495c710225
KihiaM <kihiamuthure@yahoo.com>
2019-11-06 21:48:03 +0300

    hide toggle text display

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,51 @@
+//Portfolio hover effect
+
 $(document).ready(function() {
-  $(".single-img").hover(function() {
-    $("mark").toggle();
+  $("#img1").hover(function() {
+    $(".hovertext1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#img2").hover(function() {
+    $(".hovertext2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#img3").hover(function() {
+    $(".hovertext3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#img4").hover(function() {
+    $(".hovertext4").toggle();
+  });
+});
+
+
+$(document).ready(function() {
+  $("#img5").hover(function() {
+    $(".hovertext5").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#img6").hover(function() {
+    $(".hovertext6").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#img7").hover(function() {
+    $(".hovertext7").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#img8").hover(function() {
+    $(".hovertext8").toggle();
   });
 });
 
